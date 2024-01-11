e12bcac007418c1ec6f5c5bc3be16f349187e26d
DragonXiang <11476450+dragonxiang_0@user.noreply.gitee.com>
2024-01-12 00:26:19 +0800

    pdf工具

---
+++ pdf_to_image.py
@@ -0,0 +1,28 @@
+import datetime
+import os
+import fitz  # fitz就是pip install PyMuPDF
+
+
+def py_PDF_to_Image(pdfPath, imagePath):
+    startTime_pdf2img = datetime.datetime.now()  # 开始时间
+
+    print("imagePath=" + imagePath)
+    pdfDoc = fitz.open(pdfPath)
+    for pg in range(pdfDoc.page_count):
+        page = pdfDoc[pg]
+        rotate = int(0)
+        # 每个尺寸的缩放系数为1.3，这将为我们生成分辨率提高2.6的图像。
+        # 此处若是不做设置，默认图片大小为：792X612, dpi=96
+        zoom_x = 5  # (1.33333333-->1056x816)   (2-->1584x1224)
+        zoom_y = 5
+        mat = fitz.Matrix(zoom_x, zoom_y).prerotate(rotate)
+        pix = page.get_pixmap(matrix=mat, alpha=False)
+
+        if not os.path.exists(imagePath):  # 判断存放图片的文件夹是否存在
+            os.makedirs(imagePath)  # 若图片文件夹不存在就创建
+
+        pix.save(imagePath + '/' + 'images_%s.png' % pg)
+        # pix.writePNG(imagePath + '/' + 'images_%s.png' % pg)  # 将图片写入指定的文件夹内
+
+    endTime_pdf2img = datetime.datetime.now()  # 结束时间
+    print('pdf2img时间=', (endTime_pdf2img - startTime_pdf2img).seconds)
