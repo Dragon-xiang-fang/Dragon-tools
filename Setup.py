4ada5df8e628d9a74554a58f44234f93ff962d5f
DragonXiang <11476450+dragonxiang_0@user.noreply.gitee.com>
2024-01-12 19:46:14 +0800

    更新内容

--- Setup.py
+++ Setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dragon-Tools',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     author='Dragon-xiang-fang',
     author_email="dragonxiangfang@163.com",
