4991afef55b83125173c0982b1bd0d0a5f74c6fc
DragonXiang <11476450+dragonxiang_0@user.noreply.gitee.com>
2024-01-12 17:53:45 +0800

    更新内容

--- Setup.py
+++ Setup.py
@@ -4,7 +4,7 @@ setup(
     name='dragon-tools',
     version='0.1',
     packages=find_packages(),
-    author='DragonXiang',
+    author='Dragon-xiang-fang',
     author_email="dragonxiangfang@163.com",
     description="A small package od tools",
     url='https://github.com/Dragon-xiang-fang/Dragon-tools',
